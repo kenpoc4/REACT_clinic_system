d36b57561cabc03b896d57bf0a6b445358a22ea0
Kenny Poncio <kenpoc4@gmail.com>
2021-05-03 21:16:31 -0600

    Created form, validate dates, assign ID and create register of dates registers

--- src/App.js
+++ src/App.js
@@ -1,6 +1,32 @@
+import React, { Fragment, useState } from 'react';
+import Formulario from './components/Formulario';
+
 function App() {
+
+  // Array dates
+  const [dates, saveDates] = useState( [  ] );
+
+  // Function that add new dates
+  const createdDate = date => {
+    console.log(date);
+  }
+
   return (
-    <h1>Administrador de pacientes</h1>
+    <Fragment>
+      <h1>Pet Clinic Manager</h1>
+      <div className="container">
+        <div className="row">
+          <div className="one-half column">
+            <Formulario 
+              createdDate={createdDate}
+            />
+          </div>
+          <div className="one-half column">
+            2
+          </div>
+        </div>
+      </div>
+    </Fragment>
   );
 }
 
---
+++ src/components/Formulario.js
@@ -0,0 +1,115 @@
+import React, { Fragment, useState } from 'react';
+import uuid from 'uuid/dist/v4';
+
+const Formulario = ( {createdDate} ) => {
+
+    // Create the state of dates
+    const [date, updateDate] = useState ({
+        pet:'',
+        name:'',
+        consult:'',
+        time:'',
+        symptom:''
+    });
+    const [error, updateError] = useState(false);
+
+    // Function that is running when the user writte in a input
+    const updateState = e => {
+        updateDate({
+            ...date,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    // Extract values
+    const { pet, name, consult, time, symptom } = date;
+
+    // When the user send form
+    const submitDate = e => {
+        e.preventDefault();
+
+        // Validate
+        if( pet.trim() === '' || name.trim() === '' || consult.trim() === '' || time.trim() === '' || symptom.trim() === '' ) {
+            updateError(true);
+            return;
+        }
+        updateError(false);
+
+        // Assign ID
+        date.id = uuid();
+        console.log(date);
+
+        // Create date
+        createdDate(date);
+
+        // Restart form
+    }
+
+    return ( 
+        <Fragment>
+
+            <h2>Register date</h2>
+
+            {
+                error
+                ?
+                <p className="alerta-error">All fields are Obligatory</p>
+                :
+                null
+            }
+
+            <form
+                onSubmit={submitDate}
+            >
+                <label>Baby name</label>
+                <input
+                    type="text"
+                    name="pet"
+                    className="u-full-width"
+                    placeholder="Pet's Name"
+                    onChange={updateState}
+                    valu={pet}
+                />
+                <label>Daddy/Mommy's name</label>
+                <input
+                    type="text"
+                    name="name"
+                    className="u-full-width"
+                    placeholder="Pet's owner"
+                    onChange={updateState}
+                    valu={name}
+                />
+                <label>Date</label>
+                <input
+                    type="date"
+                    name="consult"
+                    className="u-full-width"
+                    onChange={updateState}
+                    valu={consult}
+                />
+                <label>Time</label>
+                <input
+                    type="time"
+                    name="time"
+                    className="u-full-width"
+                    onChange={updateState}
+                    valu={time}
+                />
+                <label>Symptom</label>
+                <textarea
+                    className="u-full-width"
+                    name="symptom"
+                    onChange={updateState}
+                    valu={symptom}
+                ></textarea>
+                <button
+                    type="submit"
+                    className="u-full-width button-primary"
+                >Add date</button>
+            </form>
+
+        </Fragment>
+    );
+}
+ 
+export default Formulario;
